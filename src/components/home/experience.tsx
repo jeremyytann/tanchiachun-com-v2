49b0642750ea6e935cc3ce30528c9e113c809b05
Jeremy <jeremyyytannn@gmail.com>
2025-03-01 20:56:17 +0800

    build: Enhancement - Added View Full Resume button in Experience module

--- src/components/home/experience.tsx
+++ src/components/home/experience.tsx
@@ -136,6 +136,18 @@ export default function Experience() {
           development efficiency and reliability.
         </p>
       </div>
+
+      <div>
+        <a className="w-fit flex items-center color-secondary gap-3 hover:cursor-pointer" href="/resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="FusionETA (opens in a new tab)" title="FusionETA">
+          <h2 className="text-lg md:text-xl font-medium color-secondary">
+            View Full Résumé
+          </h2>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-5 w-5" aria-hidden="true">
+            <path d="M7 17L17 7M17 7H8M17 7V16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </a>
+      </div>
     </div>
   );
 }
