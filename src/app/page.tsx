2949f78386e2cda1f93d37805650021f9631c2df
Jeremy <jeremyyytannn@gmail.com>
2025-03-01 20:24:39 +0800

    build: Implementation - Experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Experience from "@/components/home/experience";
 import Introduction from "@/components/home/introduction";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-16 lg:py-24">
       <Introduction />
+      <Experience />
     </div>
   );
 }
---
+++ src/components/home/experience.tsx
@@ -0,0 +1,141 @@
+export default function Experience() {
+  return (
+    <div className="mb-16 md:mb-24 lg:mb-36 flex flex-col gap-8 md:gap-12">
+      <h1 className="text-xl md:text-2xl font-bold tracking-widest color-secondary">
+        EXPERIENCE
+      </h1>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <p className="text-sm md:text-md font-light color-primary leading-8">
+          Full-Time {'\u2022'} Feb 2023 - Present
+        </p>
+
+        <a className="w-fit flex items-center color-secondary gap-3 hover:cursor-pointer" href="https://www.getright.com.my/" target="_blank" rel="noreferrer noopener" aria-label="FusionETA (opens in a new tab)" title="FusionETA">
+          <h2 className="text-lg md:text-xl font-medium color-secondary">
+            Developer {'\u2022'} Getright Digital
+          </h2>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-5 w-5" aria-hidden="true">
+            <path d="M7 17L17 7M17 7H8M17 7V16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </a>
+
+        <p className="text-md md:text-lg font-light color-primary leading-8">
+          {'\u2022'} Played a key role in developing the all-new Sime Darby Property App,
+          utilizing React Native to create a seamless, user-friendly experience for both iOS  
+          and Android. Implemented exclusive features for property owners, including 
+          progress billing, real-time construction updates, document retrieval, and 
+          streamlined defect reporting, significantly enhancing user convenience and engagement.
+        </p>
+
+        <div className="mt-1 mb-3 flex items-center gap-x-6 gap-y-3 flex-wrap">
+          <a className="w-fit flex items-center color-secondary gap-2 hover:cursor-pointer" href="https://www.simedarbyproperty.com/" target="_blank" rel="noreferrer noopener" aria-label="Sime Darby Property (opens in a new tab)" title="Sime Darby Property">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
+            </svg>
+
+            <p className="font-medium">Sime Darby Property</p>
+          </a>
+
+          <a className="w-fit flex items-center color-secondary gap-2 hover:cursor-pointer" href="https://apps.apple.com/my/app/sime-darby-property-app/id6474657954/" target="_blank" rel="noreferrer noopener" aria-label="Sime Darby Property iOS App (opens in a new tab)" title="Sime Darby Property iOS App">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
+            </svg>
+
+            <p className="font-medium">iOS</p>
+          </a>
+
+          <a className="w-fit flex items-center color-secondary gap-2 hover:cursor-pointer" href="https://play.google.com/store/apps/details?id=com.simedarbyproperty.sdpappv2&hl=en/" target="_blank" rel="noreferrer noopener" aria-label="Sime Darby Property Android App (opens in a new tab)" title="Sime Darby Property Android App">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
+            </svg>
+
+            <p className="font-medium">Android</p>
+          </a>
+        </div>
+
+        <p className="text-md md:text-lg font-light color-primary leading-8">
+          {'\u2022'} Contributed to the development and enhancement of Skillsture, 
+          a cross-platform livestream training platform for Web, iOS, and Android.
+          Optimized performance and user experience by leveraging React, Flutter, 
+          MongoDB, and Express. Integrated Stripe API for seamless payment processing 
+          and implemented Zoom Client SDK to enable livestream capabilities, improving 
+          interactivity and the overall learning experience.
+        </p>
+
+        <div className="mt-1 mb-3 flex items-center gap-x-6 gap-y-3 flex-wrap">
+          <a className="w-fit flex items-center color-secondary gap-2 hover:cursor-pointer" href="https://www.skillsture.com/" target="_blank" rel="noreferrer noopener" aria-label="Skillsture (opens in a new tab)" title="Skillsture">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
+            </svg>
+
+            <p className="font-medium">Skillsture</p>
+          </a>
+
+          <a className="w-fit flex items-center color-secondary gap-2 hover:cursor-pointer" href="https://apps.apple.com/my/app/skillsture/id6444467270/" target="_blank" rel="noreferrer noopener" aria-label="Skillsture iOS App (opens in a new tab)" title="Skillsture iOS App">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
+            </svg>
+
+            <p className="font-medium">iOS</p>
+          </a>
+
+          <a className="w-fit flex items-center color-secondary gap-2 hover:cursor-pointer" href="https://play.google.com/store/apps/details?id=com.flutter.skillsture&hl=en/" target="_blank" rel="noreferrer noopener" aria-label="Skillsture Android App (opens in a new tab)" title="Sime Darby Property Android App">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
+            </svg>
+
+            <p className="font-medium">Android</p>
+          </a>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <p className="text-sm md:text-md font-light color-primary leading-8">
+          Intern {'\u2022'} Aug 2022 - Jan 2023
+        </p>
+
+        <a className="w-fit flex items-center color-secondary gap-3 hover:cursor-pointer" href="https://fusioneta.com/" target="_blank" rel="noreferrer noopener" aria-label="FusionETA (opens in a new tab)" title="FusionETA">
+          <h2 className="text-lg md:text-xl font-medium color-secondary">
+            Full Stack Developer {'\u2022'} FusionETA
+          </h2>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-5 w-5" aria-hidden="true">
+            <path d="M7 17L17 7M17 7H8M17 7V16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </a>
+
+        <p className="text-md md:text-lg font-light color-primary leading-8">
+          {'\u2022'} Assisted in the development and ongoing maintenance of codebases 
+          for Altomate and multiple other projects, leveraging modern technologies 
+          such as React, Angular, Express, and Google Cloud to deliver scalable 
+          and efficient solutions.
+        </p>
+
+        <div className="mt-1 mb-3 flex items-center gap-x-6 gap-y-3 flex-wrap">
+          <a className="w-fit flex items-center color-secondary gap-2 hover:cursor-pointer" href="https://altomate.io/" target="_blank" rel="noreferrer noopener" aria-label="Altomate (opens in a new tab)" title="Altomate">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
+            </svg>
+
+            <p className="font-medium">Altomate</p>
+          </a>
+        </div>
+
+        <p className="text-md md:text-lg font-light color-primary leading-8">
+          {'\u2022'} Designed and implemented a CI/CD pipeline using GitHub Actions 
+          and Google Firebase, automating build and deployment processes across 
+          multiple environments for Altomate and other projects, improving 
+          development efficiency and reliability.
+        </p>
+      </div>
+    </div>
+  );
+}
