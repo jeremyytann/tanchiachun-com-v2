909d6bc127367cb2424cffe4d0621c676bd28fb6
Jeremy <jeremyyytannn@gmail.com>
2025-03-01 20:36:20 +0800

    build: Implementation - Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Experience from "@/components/home/experience";
+import Projects from "@/components/home/projects";
 import Introduction from "@/components/home/introduction";
 import Image from "next/image";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-16 lg:py-24">
       <Introduction />
       <Experience />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/home/projects.tsx
@@ -0,0 +1,110 @@
+export default function Projects() {
+  return (
+    <div className="mb-16 md:mb-24 lg:mb-36 flex flex-col gap-8 md:gap-12">
+      <h1 className="text-xl md:text-2xl font-bold tracking-widest color-secondary">
+        PROJECTS
+      </h1>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <a className="w-fit flex items-center color-secondary gap-3 hover:cursor-pointer" href="https://techconcept.my/" target="_blank" rel="noreferrer noopener" aria-label="FusionETA (opens in a new tab)" title="FusionETA">
+          <h2 className="text-lg md:text-xl font-medium color-secondary">
+            Tech Concept
+          </h2>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-5 w-5" aria-hidden="true">
+            <path d="M7 17L17 7M17 7H8M17 7V16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </a>
+
+        <p className="text-md md:text-lg font-light color-primary leading-8">
+          Spearheaded the development of Tech Concept, an e-commerce platform 
+          integrating seamlessly with CommercePay, Fiuu, and Payex for secure payments. 
+          Implemented an intuitive admin dashboard with advanced user role management, 
+          including a clock-in/out and leave system for precise employee tracking. 
+          The platform also features dynamic content management for updates and user-friendly 
+          shopping features, contributing to an enhanced overall user experience.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <a className="w-fit flex items-center color-secondary gap-3 hover:cursor-pointer" href="https://www.secondsbooth.com/" target="_blank" rel="noreferrer noopener" aria-label="FusionETA (opens in a new tab)" title="FusionETA">
+          <h2 className="text-lg md:text-xl font-medium color-secondary">
+            Seconds Photobooth
+          </h2>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-5 w-5" aria-hidden="true">
+            <path d="M7 17L17 7M17 7H8M17 7V16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </a>
+
+        <p className="text-md md:text-lg font-light color-primary leading-8">
+          Contributed to the development of Seconds Photobooth, leveraging React, 
+          Express, and AWS Lambda. Integrated iPay88 for seamless QR code payments, 
+          enhancing the efficiency of the pre-payment process for users engaging 
+          with the photo booth services.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <a className="w-fit flex items-center color-secondary gap-3 hover:cursor-pointer" href="https://www.netglue.com.my/" target="_blank" rel="noreferrer noopener" aria-label="FusionETA (opens in a new tab)" title="FusionETA">
+          <h2 className="text-lg md:text-xl font-medium color-secondary">
+            NGT Mobile {'\u2022'} Netglue
+          </h2>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-5 w-5" aria-hidden="true">
+            <path d="M7 17L17 7M17 7H8M17 7V16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </a>
+
+        <p className="text-md md:text-lg font-light color-primary leading-8">
+          Led the development of the NGT Mobile, a mobile application for both iOS 
+          and Android built with Flutter. Implemented a personalized login screen 
+          and seamlessly integrated an in-app web view for direct access to web portal
+          services. Elevated user experience with a customized network error screen 
+          and integrated Firebase for efficient push notification services, 
+          ensuring timely updates and heightened user engagement.
+        </p>
+
+        <div className="mt-1 flex items-center gap-x-6 gap-y-3 flex-wrap">
+          <a className="w-fit flex items-center color-secondary gap-2 hover:cursor-pointer" href="https://apps.apple.com/id/app/ngt-mobile-v6/id6474528807/" target="_blank" rel="noreferrer noopener" aria-label="NGT Mobile iOS App (opens in a new tab)" title="NGT Mobile iOS App">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
+            </svg>
+
+            <p className="font-medium">iOS</p>
+          </a>
+
+          <a className="w-fit flex items-center color-secondary gap-2 hover:cursor-pointer" href="https://play.google.com/store/apps/details?id=com.netglue.iotngtmobile" target="_blank" rel="noreferrer noopener" aria-label="NGT Mobile Android App (opens in a new tab)" title="NGT Mobile Android App">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
+              <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
+            </svg>
+
+            <p className="font-medium">Android</p>
+          </a>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <a className="w-fit flex items-center color-secondary gap-3 hover:cursor-pointer" href="https://v1.tanchiachun.com/" target="_blank" rel="noreferrer noopener" aria-label="tanchiachun.com v1 (opens in a new tab)" title="tanchiachun.com v1">
+          <h2 className="text-lg md:text-xl font-medium color-secondary">
+            tanchiachun.com (v1)
+          </h2>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="h-5 w-5" aria-hidden="true">
+            <path d="M7 17L17 7M17 7H8M17 7V16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </a>
+
+        <p className="text-md md:text-lg font-light color-primary leading-8">
+          Designed, developed, and hosted my first personal website using Figma, React, 
+          and Firebase Hosting to showcase my portfolio, projects, and technical skills. 
+          Integrated a seamless light/dark mode for an enhanced user experience. This project 
+          served as a foundation for establishing my professional online presence, 
+          demonstrating both my design and development capabilities.
+        </p>
+      </div>
+    </div>
+  );
+}
