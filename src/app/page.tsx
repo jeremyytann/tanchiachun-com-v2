280ab18b03300ff23c4223dc1a999f578bccfa26
Jeremy <jeremyyytannn@gmail.com>
2025-03-01 23:43:29 +0800

    build: Implementation - Skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Experience from "@/components/home/experience";
 import Projects from "@/components/home/projects";
 import Introduction from "@/components/home/introduction";
 import Footer from "@/components/home/footer";
+import Skills from "@/components/home/skills";
 
 export default function Home() {
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-16 lg:py-24">
       <Introduction />
       <Experience />
+      <Skills />
       <Projects />
       <Footer />
     </div>
---
+++ src/components/home/skills.tsx
@@ -0,0 +1,209 @@
+export default function Skills() {
+  return (
+    <div className="mb-16 md:mb-24 lg:mb-36 flex flex-col gap-8 md:gap-12">
+      <h1 className="text-xl md:text-2xl font-bold tracking-widest color-secondary">
+        SKILLS
+      </h1>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <h2 className="text-lg md:text-xl font-medium tracking-wider color-secondary ">
+          PROGRAMMING LANGUAGES
+        </h2>
+
+        <div className="flex gap-x-3 gap-y-3 md:gap-x-4 md:gap-y-4 flex-wrap">
+          <div className="flex gap-2 items-center px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              JavaScript
+            </p>
+          </div>
+
+          <div className="flex gap-2 items-center px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              TypeScript
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              HTML
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              CSS
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Dart
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Python
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <h2 className="text-lg md:text-xl font-medium tracking-wider color-secondary ">
+          LIBRARIES & FRAMEWORKS
+        </h2>
+
+        <div className="flex gap-x-3 gap-y-3 md:gap-x-4 md:gap-y-4 flex-wrap">
+          <div className="flex gap-2 items-center px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              React
+            </p>
+          </div>
+
+          <div className="flex gap-2 items-center px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Next.js
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Tailwind CSS
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Angular
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              React Native
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Flutter
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Node.js
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Express
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Hono
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              GraphQL
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <h2 className="text-lg md:text-xl font-medium tracking-wider color-secondary ">
+          CLOUD & DATABASES
+        </h2>
+
+        <div className="flex gap-x-3 gap-y-3 md:gap-x-4 md:gap-y-4 flex-wrap">
+          <div className="flex gap-2 items-center px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              AWS
+            </p>
+          </div>
+
+          <div className="flex gap-2 items-center px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Google Cloud
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Heroku
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Firebase
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              MongoDB
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              MySQL
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              PostgreSQL
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 md:gap-4">
+        <h2 className="text-lg md:text-xl font-medium tracking-wider color-secondary ">
+          DEVOPS & TOOLS
+        </h2>
+
+        <div className="flex gap-x-3 gap-y-3 md:gap-x-4 md:gap-y-4 flex-wrap">
+          <div className="flex gap-2 items-center px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              GitHub
+            </p>
+          </div>
+
+          <div className="flex gap-2 items-center px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              GitLab
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Docker
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Postman
+            </p>
+          </div>
+
+          <div className="px-4 py-1 bg-zinc-800 rounded-full">
+            <p className="text-md md:text-lg font-light color-background">
+              Figma
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
