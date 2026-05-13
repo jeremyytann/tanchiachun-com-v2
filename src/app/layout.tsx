217d3e651e56e6992ea499200bdcb7eeb3541cbe
Jeremy <jeremyyytannn.work@gmail.com>
2026-05-14 05:38:10 +0800

    fix: status bar color issue on iOS devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={'bg-[#f7f5f1]'}>
       <body className={'antialiased'}>
         {children}
       </body>
